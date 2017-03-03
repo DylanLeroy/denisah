05f68132fd9e9f593c84de35341da3cd4789129e
DylanLeroy <dydydylan599@gmail.com>
2017-03-03 11:32:30 +0100

    Add files via upload

--- js.js
+++ js.js
@@ -1,13 +1,17 @@
 ﻿$(document).ready(function() {
             var audioElement = document.createElement('audio');
+            var x=2;
             audioElement.setAttribute('src', 'ah.mp3');
-
             $.get();
             elementaudio = document.getElementById("kohlanta");
 			elementaudio.volume=0.1;
 			audioElement.volume=1;
-
+			
+			
             $('#logo').click(function() {
                 audioElement.play();
             });
+  			$("#epic").click(function() {
+  				document.getElementById("epic").play();
+  			});
         });
